003fce5c47dafb86610316e2c074ff06653a2143
PaulBender <paulbender@live.com>
2014-12-12 12:37:06 -0500

    Bump to version v1.1

--- inc/plugins/usernameapprovalhistory.php
+++ inc/plugins/usernameapprovalhistory.php
@@ -75,7 +75,8 @@ function usernameapprovalhistory_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.0",
+		"version"			=> "1.1",
+		"codename"			=> "usernameapprovalhistory",
 		"compatibility"		=> "18*"
 	);
 }
